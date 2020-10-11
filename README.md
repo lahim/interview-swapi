58cdb1fd949c0fdf97db8339a4ccf5ecc5e2beac
Michał Krawczak <michal@open8.co>
2020-10-11 22:45:00 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# interview-swapi
+# Interview SWAPI
+Project was copied from https://github.com/phalt/swapi
+
+# Befor first run
+```bash
+python3 -m venv venv
+. venv/bin/activate
+make install
+make build
+make load_data
+```
+
+# How to run?
+```bash
+make serve
+```
+SWAPI should be available under below url:
+```bash
+http://localhost:5000
+```
+
---
+++ swapi/urls.py
@@ -0,0 +1,36 @@
+"""swapi URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework import routers
+
+from resources import views
+
+router = routers.DefaultRouter()
+
+router.register(r'people', views.PeopleViewSet)
+router.register(r'planets', views.PlanetViewSet)
+router.register(r'films', views.FilmViewSet)
+router.register(r'species', views.SpeciesViewSet)
+router.register(r'vehicles', views.VehicleViewSet)
+router.register(r'starships', views.StarshipViewSet)
+
+urlpatterns = [
+    path(r'admin/', admin.site.urls),
+
+    path(r'api/', include(router.urls)),
+]
